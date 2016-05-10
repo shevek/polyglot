be478886ccf984340db1dab19805cbeb5ebdd085
Shevek <shevek@anarres.org>
2016-05-10 09:29:58 -0700

    Note which file failed.

--- polyglot-core/src/test/java/org/anarres/polyglot/PolyglotTest.java
+++ polyglot-core/src/test/java/org/anarres/polyglot/PolyglotTest.java
@@ -34,7 +34,7 @@ public class PolyglotTest extends AbstractPolyglotTest {
         if (file.getName().equals("php4.sablecc"))
             engine.setOption(Option.ALLOWMASKEDTOKENS, true);
         if (!engine.run())
-            fail("Polyglot failed:\n" + engine.getErrors().toString(engine.getInput()));
+            fail("Polyglot failed on " + file + ":\n" + engine.getErrors().toString(engine.getInput()));
     }
 
     @Test
