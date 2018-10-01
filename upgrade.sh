2f349267950746d6c55c04dc62324483465ae145
Shevek <shevek@anarres.org>
2018-09-30 21:38:26 -0700

    Upgrade: No animalSniffer any more.

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
 
-./gradlew --daemon clean install -xjavadoc -xanimalSniffer
+./gradlew --daemon clean install -xjavadoc
 ./gradlew --stop
 jps -l | grep GradleDaemon | cut -f1 -d' ' | xargs -rt kill
